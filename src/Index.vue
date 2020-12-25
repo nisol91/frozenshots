2040b9a548d0aee10646cd53cffb578a9a31fe5c
nisol91 <nicolasolzi91@gmail.com>
2020-12-25 20:22:19 +0100

    first

---
+++ src/Index.vue
@@ -0,0 +1,578 @@
+<template>
+  <!-- nell index è comodo mettere la navigazione dell'app -->
+  <div>
+    <v-app>
+      <div class="mainBox" v-scroll="scrolled">
+        <!-- header menu -->
+        <div
+          class="mTop included"
+          v-if="loaded"
+          :class="[{ menuBkg: scroll > 200 }]"
+        >
+          <div class="mTopEl mCont">
+            <div class="mLine" :class="[{ bkgBlack: notHome }]"></div>
+            <router-link class="menuEl" :to="{ name: 'mContacts' }">
+              <div
+                class="mContText"
+                :style="{ color: notHome ? 'black' : 'white' }"
+              >
+                CONTACTS
+              </div>
+            </router-link>
+          </div>
+          <router-link :to="{ name: 'mHome' }" class="mTopEl mLogo">
+            <div style="color: red">中文</div>
+          </router-link>
+          <div class="mTopEl mMenu" :class="[{ colorBlack: notHome }]">
+            <div
+              class="menuOpen"
+              :class="[{ showMenu: menu, showMenuLong: isLoggedIn && menu }]"
+              v-click-outside="{
+                handler: onClickOutsideSideBar,
+                include: include,
+              }"
+            >
+              <router-link :to="{ name: 'mHome' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mHome.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mHome')"
+                >
+                  HOME
+                </div>
+              </router-link>
+              <div class="menuElDivider">/</div>
+              <router-link :to="{ name: 'mWeAre' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mAbout.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mAbout')"
+                >
+                  WE ARE
+                </div>
+              </router-link>
+              <div class="menuElDivider">/</div>
+              <router-link :to="{ name: 'mWeDo' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mContents.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mContents')"
+                >
+                  WE DO
+                </div>
+              </router-link>
+              <div class="menuElDivider">/</div>
+              <router-link :to="{ name: 'mWeTell' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mBlog.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mBlog')"
+                >
+                  WE TELL
+                </div>
+              </router-link>
+              <div class="menuElDivider">/</div>
+              <!-- <router-link :to="{ name: 'mShop' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mShop.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mShop')"
+                >
+                  SHOP
+                </div>
+              </router-link> -->
+              <!-- <div class="menuElDivider">/</div> -->
+              <router-link :to="{ name: 'mContacts' }" class="menuEl">
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.mContacts.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('mContacts')"
+                >
+                  CONTACTS
+                </div>
+              </router-link>
+              <!-- <div class="menuElDivider">/</div> -->
+
+              <!-- login -->
+              <!-- <router-link
+                v-if="!isLoggedIn"
+                :to="{ name: 'login' }"
+                class="menuEl"
+              >
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.login.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('login')"
+                >
+                  Login
+                </div>
+              </router-link> -->
+
+              <!-- logout -->
+              <!-- <router-link
+                v-if="isLoggedIn"
+                :to="{ name: 'mHome' }"
+                class="menuEl"
+              >
+                <div
+                  class="menuLine"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="logout"
+                >
+                  Logout
+                </div>
+              </router-link>
+              <div class="menuElDivider" v-if="isLoggedIn">/</div>
+              -->
+              <!-- settings -->
+              <!--
+              <router-link
+                v-if="isLoggedIn"
+                :to="{ name: 'userProfile' }"
+                class="menuEl"
+              >
+                <div
+                  class="menuLine"
+                  :class="[{ menuLineShow: menuEl.userProfile.val }]"
+                  :style="{ background: notHome ? 'black' : 'white' }"
+                ></div>
+                <div
+                  :style="{ color: notHome ? 'black' : 'white' }"
+                  @click="selectEl('userProfile')"
+                >
+                  Settings
+                </div>
+              </router-link> -->
+              <!--  -->
+            </div>
+            <div
+              class="mMenuText fade-in fade-out"
+              :class="[{ hideMenu: menu }]"
+            >
+              MENU
+            </div>
+
+            <div
+              v-if="!menu"
+              class="mLineMenu fade-in fade-out"
+              @click="toggleMenu"
+              :class="[{ bkgBlack: notHome, bkgBlack_2: notHome }]"
+            >
+              <div
+                class="mLineMenuCenterLine"
+                :class="[{ bkgBlack: notHome }]"
+              ></div>
+            </div>
+            <v-icon
+              v-if="menu"
+              @click="toggleMenu"
+              class="menuCross fade-in fade-out"
+              :style="{ color: notHome ? 'black' : 'white' }"
+              >mdi-close</v-icon
+            >
+          </div>
+        </div>
+        <!--  -->
+        <transition name="fade-in">
+          <router-view></router-view>
+        </transition>
+
+        <!-- footer -->
+        <div class="mFooter" v-if="notHome">
+          <div class="fBody">
+            <div class="fbCol">
+              <div class="">logo</div>
+              <div class="">FrozenShots 2020</div>
+            </div>
+
+            <div class="fbCol">
+              <div class="sitemapText">sitemap</div>
+              <router-link :to="{ name: 'mHome' }"
+                ><div class="sitemapEl">home</div></router-link
+              >
+              <router-link :to="{ name: 'mWeAre' }"
+                ><div class="sitemapEl">weare</div></router-link
+              >
+              <router-link :to="{ name: 'mWeDo' }"
+                ><div class="sitemapEl">wedo</div></router-link
+              >
+              <router-link :to="{ name: 'mWeTell' }"
+                ><div class="sitemapEl">wetell</div></router-link
+              >
+              <router-link :to="{ name: 'mContacts' }"
+                ><div class="sitemapEl">contacts</div></router-link
+              >
+            </div>
+          </div>
+        </div>
+
+        <!-- for some type of notification -->
+        <global-message
+          v-if="globalMessage"
+          :globalMex="globalMessage"
+          :error="false"
+        ></global-message>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import * as easings from "vuetify/es5/services/goto/easing-patterns";
+import axios from "axios";
+import firebase from "firebase";
+import store from "./store";
+
+export default {
+  // this can be used alternatively to the meta in the head of welcome.blade.php page, for seo purpose
+  metaInfo: {
+    title: "FrozenShots",
+    titleTemplate: "%s - Freeze your moments",
+    htmlAttrs: {
+      lang: "en",
+      amp: true,
+    },
+  },
+  data() {
+    return {
+      // lastSearch: this.$store.state.lastSearch,
+      menuOpen: false,
+      loaded: false,
+      scroll: null,
+    };
+  },
+  async created() {
+    this.loaded = false;
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.VUE_APP_DB_ENV);
+
+    // carico l utente firebase dopo il refresh
+    this.$store.dispatch("loadFirebaseUserAfterRefresh");
+    this.$store.commit("getUserOnRefresh");
+
+    // carico il carrello al refresh
+    this.$store.dispatch("loadBasketOnRefresh");
+
+    // prendo l userRole nello storage
+    this.$store.commit("getUserRole");
+
+    setTimeout(() => {
+      this.loaded = true;
+    }, 2000);
+  },
+  methods: {
+    onClickOutsideSideBar() {
+      this.$store.commit("closeMenu");
+    },
+    include() {
+      return [document.querySelector(".included")];
+    },
+    scrolled(position) {
+      this.scroll = position;
+    },
+    // menu
+    selectEl(value) {
+      console.log("select el");
+      this.$store.commit("selectEl", value);
+    },
+    toggleMenu() {
+      this.$store.commit("toggleMenu");
+    },
+    closeMenu() {
+      this.$store.commit("closeMenu");
+    },
+    async logout() {
+      this.$store.dispatch("signOut");
+    },
+  },
+  watch: {
+    $route(to, from) {
+      console.log(this.$route);
+      var value = this.$route.name;
+      console.log("select el");
+      var menu = this.menuEl;
+      for (const el in menu) {
+        if (menu[el].slug.includes(value)) {
+          menu[el].val = true;
+        } else {
+          menu[el].val = false;
+        }
+      }
+      this.closeMenu();
+    },
+  },
+  computed: {
+    ...mapState({
+      isLoggedIn: "isLoggedIn",
+      userRole: "userRole",
+      globalMessage: "globalMessage",
+      isMetamorphosis: "isMetamorphosis",
+      menu: "menu",
+      menuEl: "menuEl",
+      notHome: "notHome",
+    }),
+    ...mapGetters({ itemsInBasket: "itemsInBasket" }),
+  },
+};
+</script>
+<style lang="scss">
+.sitemapText {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.sitemapEl {
+  cursor: pointer;
+  color: white !important;
+  text-decoration: none !important;
+}
+.fBody {
+  width: 100%;
+  height: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 30px;
+}
+.mFooter {
+  width: 100%;
+  height: 250px;
+  background: rgb(41, 41, 41);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+}
+.colorBlack {
+  color: black !important;
+}
+.bkgBlack {
+  border-bottom: 2px solid black !important;
+}
+.bkgBlack_2 {
+  border-top: 2px solid black !important;
+}
+.menuBkg {
+  background: rgba(167, 167, 167, 0.596);
+  transition: 1s;
+}
+.mTop {
+  width: 100vw;
+  height: 100px;
+  transition: 1s;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  top: 0px;
+  z-index: 9800;
+  .mTopEl {
+    width: 33%;
+    display: flex;
+    align-items: center;
+  }
+  .mLogo {
+    justify-content: center;
+    color: red;
+    font-weight: bold;
+    font-size: 30px;
+    cursor: pointer;
+    z-index: 9994;
+    &:hover {
+      text-decoration: none !important;
+    }
+  }
+  .mCont {
+    justify-content: flex-start;
+    font-weight: bold;
+    color: white;
+    z-index: 9999;
+
+    .mContText {
+      margin-left: 10px;
+      transition: 1s;
+      cursor: pointer;
+      &:hover {
+        margin-left: 30px;
+        transition: 1s;
+      }
+    }
+  }
+  .mMenu {
+    justify-content: flex-end;
+    z-index: 9999;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+
+    .mMenuText {
+      margin-right: 10px;
+    }
+    .hideMenu {
+      display: none;
+    }
+    .menuEl {
+      font-size: 14px;
+      margin: 0 3px;
+    }
+  }
+}
+
+.menuOpen {
+  position: fixed;
+  top: 40px;
+  right: -350px;
+  transition: 0.5s;
+  display: flex;
+  justify-content: space-between;
+  width: 350px;
+}
+.showMenu {
+  transition: 0.5s;
+  right: 100px;
+}
+.showMenuLong {
+  right: 260px;
+}
+.menuCross {
+  margin-right: 10px;
+  font-size: 40px !important;
+}
+.mLineMenu {
+  width: 50px;
+  height: 20px;
+  border-top: 2px solid white;
+  border-bottom: 2px solid white;
+  position: relative;
+  transition: 1s;
+
+  &:hover {
+    // background: rgba(52, 55, 66, 0.356);
+    margin-right: 5px;
+    transition: 0.5s;
+  }
+  .mLineMenuCenterLine {
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    top: 7px;
+    border-bottom: 2px solid white;
+  }
+}
+// ##
+@media (max-width: 1050px) {
+  .mTop {
+    background: rgba(255, 255, 255, 0.329);
+  }
+  .menuBkg {
+    background: rgba(167, 167, 167, 0.596);
+    transition: 1s;
+  }
+}
+// ##
+@media (max-width: 800px) {
+  .menuOpen {
+    width: 300px;
+    top: 80px;
+  }
+
+  .showMenu,
+  .showMenuLong {
+    transition: 0.5s;
+    right: 50px;
+  }
+  .mTop {
+    .mMenu {
+      .menuEl {
+        font-size: 10px;
+        margin: 0 3px;
+        .menuLine {
+          top: -3px;
+        }
+      }
+    }
+  }
+
+  .menuElDivider {
+    display: none;
+  }
+  .mContText {
+    &:hover {
+      margin-left: 0px !important;
+      transition: 1s;
+    }
+  }
+}
+// ##
+@media (max-width: 400px) {
+  .mFooter {
+    height: 300px;
+  }
+  .fBody {
+    flex-direction: column;
+  }
+  .fbCol {
+    text-align: center;
+  }
+  .sitemapText {
+    margin-top: 20px;
+    font-size: 15px;
+  }
+  .sitemapEl {
+    font-size: 10px;
+    margin-top: 5px;
+  }
+  .menuOpen {
+    width: 200px;
+  }
+  .showMenu,
+  .showMenuLong {
+    transition: 0.5s;
+    right: 70px;
+  }
+  .mTop {
+    .mMenu {
+      .menuEl {
+        font-size: 8px;
+        margin: 0 2px;
+        .menuLine {
+          top: -3px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/metamorphosis/MWeDo.vue
@@ -0,0 +1,305 @@
+<template>
+  <div class="mWorksBox">
+    <div class="wFilters">
+      <div
+        class="wFi"
+        :class="[
+          {
+            catSelectedWorks: 'photo_video' == catSelected,
+          },
+        ]"
+        @click="filterTax(48, 'categories', 'photo_video')"
+      >
+        PHOTO/VIDEO
+      </div>
+      <div
+        class="wFi"
+        :class="[
+          {
+            catSelectedWorks: 'dev' == catSelected,
+          },
+        ]"
+        @click="filterTax(47, 'categories', 'dev')"
+      >
+        DEV
+      </div>
+      <div
+        class="wFi"
+        :class="[
+          {
+            catSelectedWorks: 'graphics' == catSelected,
+          },
+        ]"
+        @click="filterTax(49, 'categories', 'graphics')"
+      >
+        GRAPHICS
+      </div>
+    </div>
+    <q-circular-progress
+      indeterminate
+      size="45px"
+      :thickness="1"
+      color="grey-8"
+      track-color="blue-grey-14"
+      class="q-ma-md loaderWorks"
+      v-if="!works"
+    />
+    <div
+      class="work"
+      v-for="(work, i) in works"
+      :key="i + `_work`"
+      @click="pushSingleWork(work.id)"
+      :class="[
+        {
+          pushedSingleWorkClass: pushedSingleWork,
+        },
+      ]"
+    >
+      <v-img
+        v-if="work.featured_image_src"
+        :src="work.featured_image_src"
+        class="grey lighten-2 workImage"
+        :aspect-ratio="16 / 9"
+        @mouseover="showByIndex = i"
+        @mouseout="showByIndex = null"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+        <div
+          class="workImageOverlay"
+          :class="[
+            {
+              'fade-out-worktitle': showByIndex !== i,
+              'fade-in': showByIndex === i,
+            },
+          ]"
+        ></div>
+        <div
+          class="imgTitle"
+          :class="[
+            {
+              'fade-out-worktitle': showByIndex !== i,
+              'fade-in': showByIndex === i,
+            },
+          ]"
+        >
+          {{ work.title.rendered }}
+        </div>
+      </v-img>
+    </div>
+  </div>
+</template>
+<script>
+import { db, Timestamp, GeoPoint } from "../../main";
+import firebase from "firebase";
+import VueMoment from "vue-moment";
+import { mapState, mapGetters } from "vuex";
+import axios from "axios";
+import _ from "lodash";
+
+export default {
+  data() {
+    return {
+      works: null,
+      catSelected: "",
+      env: process.env.VUE_APP_DB_ENV,
+      adminCode: null,
+      loaded: false,
+      showByIndex: null,
+      pushedSingleWork: false,
+    };
+  },
+  created() {
+    this.getWorks();
+    this.$store.commit("selectEl", "mContents");
+    setTimeout(() => {
+      this.$store.commit("toggleHomeMenuColor", true);
+    }, 2000);
+  },
+  methods: {
+    pushSingleWork(workId) {
+      this.pushedSingleWork = true;
+      setTimeout(() => {
+        this.$router.push({
+          name: "mSingleWork",
+          params: { id: workId },
+        });
+      }, 2000);
+    },
+    async getWorks() {
+      this.loaded = false;
+      try {
+        var works = (
+          await axios.get(
+            `https://endorphinoutdoor.com/wp-json/wp/v2/posts?categories=46`
+          )
+        ).data;
+        await this.getOtherFields(works);
+      } catch (error) {
+        console.log(error);
+      }
+
+      this.loaded = true;
+    },
+    async getOtherFields(works) {
+      // get other fields
+      for (const work of works) {
+        // non piu necessario #######
+        // if (work.featured_media !== 0) {
+        //   work["featured_media_url"] = (
+        //     await axios.get(
+        //       `https://endorphinoutdoor.com/wp-json/wp/v2/media/${work.featured_media}`
+        //     )
+        //   ).data.source_url;
+        // }
+        // add custom field
+        // var acf = (
+        //   await axios.get(
+        //     `https://endorphinoutdoor.com/wp-json/acf/v3/posts/${work.id}`
+        //   )
+        // ).data.acf;
+        // work["acf"]["client_name"] = acf.client_name;
+        // work["acf"]["year"] = acf.year;
+        // work["acf"]["technologies"] = acf.technologies;
+      }
+      this.works = works;
+    },
+    async filterTax(tax, type, catSel) {
+      this.catSelected = catSel;
+      this.works = null;
+      this.loaded = false;
+      try {
+        var works = (
+          await axios.get(
+            `https://endorphinoutdoor.com/wp-json/wp/v2/posts?${type}=${tax}`
+          )
+        ).data;
+        await this.getOtherFields(works);
+      } catch (error) {
+        console.log(error);
+      }
+      this.loaded = true;
+    },
+  },
+  computed: {
+    ...mapState({
+      user: "user",
+      userRole: "userRole",
+      menuEl: "menuEl",
+    }),
+  },
+};
+</script>
+<style lang="scss">
+@import "../../sass/_variables.scss";
+.fade-out-worktitle {
+  -webkit-animation: fade-out 2s ease-out both;
+  animation: fade-out 2s ease-out both;
+}
+.pushedSingleWorkClass {
+  opacity: 0;
+  transition: 3s;
+}
+.mWorksBox {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  padding: 100px 0;
+}
+.work {
+  width: 100%;
+  height: 300px;
+  cursor: pointer;
+}
+.workImage {
+  width: 100%;
+  max-height: 100%;
+  transition: 1s;
+  position: relative;
+}
+.workImageOverlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(51, 51, 51, 0.575);
+}
+.imgTitle {
+  transition: 1s;
+  position: absolute;
+  top: 80%;
+  right: 12%;
+  text-align: right;
+  font-size: 27px;
+  color: white;
+}
+.wFilters {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.wFi {
+  width: calc(100vw / 3);
+  height: 100%;
+  border: 1px solid rgba(223, 223, 223, 0.63);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: 1s;
+
+  &:hover {
+    background: rgba(182, 182, 182, 0.521);
+    transition: 1s;
+  }
+}
+.loaderWorks {
+  margin-top: 30vh !important;
+}
+.catSelectedWorks {
+  background: $primary-color;
+  transition: 1s;
+  padding: 3px;
+}
+// ##
+@media (max-width: 650px) {
+  .work {
+    height: 200px;
+  }
+  .wFilters {
+    flex-direction: column;
+    height: 150px;
+  }
+  .wFi {
+    width: 100%;
+    height: 50px;
+  }
+  .imgTitle {
+    transition: 1s;
+    position: absolute;
+    top: 80%;
+    right: 8%;
+    text-align: right;
+    font-size: 16px;
+    color: white;
+  }
+}
+// ##
+@media (max-width: 400px) {
+  .work {
+    height: 150px;
+  }
+}
+</style>
